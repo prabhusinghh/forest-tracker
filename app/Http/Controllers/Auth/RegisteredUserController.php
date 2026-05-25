77b174824f1bd19e582da64333a3d758e5577048
Prabhu Singh <iamprabhusingh543@gmail.com>
2026-05-26 03:38:36 +0530

    Temporary auto admin registration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -37,10 +37,12 @@ class RegisteredUserController extends Controller
         ]);
 
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+    'name' => $request->name,
+    'email' => $request->email,
+    'password' => Hash::make($request->password),
+    'role' => 'admin',
+    'is_approved' => true,
+]);
 
         event(new Registered($user));
 
