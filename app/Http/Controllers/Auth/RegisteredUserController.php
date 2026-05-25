3b7a9174b6b8b66fd7528028e13291f9f8104a15
Prabhu Singh <iamprabhusingh543@gmail.com>
2026-05-26 03:44:11 +0530

    Temporary admin registration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,8 +40,8 @@ class RegisteredUserController extends Controller
     'name' => $request->name,
     'email' => $request->email,
     'password' => Hash::make($request->password),
-    'role' => 'admin',
-    'is_approved' => true,
+    'role' => 'conservationist',
+'is_approved' => false,
 ]);
 
         event(new Registered($user));
