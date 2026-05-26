a37166a0b6f042ea3be10d5bc46581a965644942
Prabhu Singh <iamprabhusingh543@gmail.com>
2026-05-26 22:45:48 +0530

    fix: trust proxies to resolve 419 CSRF error on Render deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\TrustProxies;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
      ->withMiddleware(function ($middleware) {
+
+    $middleware->trustProxies(at: '*');
 
     $middleware->alias([
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
